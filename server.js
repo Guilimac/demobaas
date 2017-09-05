40a6ebb3dbeca186c46774e23208c957e0881892
logonrm <logonrm@fiap.com.br>
2017-09-04 22:33:52 -0300

    models

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
+const api = require('./api');
 
 let port = process.env.PORT || 3030
 
+
+app.use('/api', api);
 app.get('/', (req,res)=>{
     res.send('hello word');
 })
